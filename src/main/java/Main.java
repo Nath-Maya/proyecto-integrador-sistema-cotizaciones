83bf90f3243cd92fb833a4894da20eab8714f8eb
Nath-Maya <natha.maya.ramirez93@gmail.com>
2026-03-18 19:20:32 -0500

    Agregar clase Main y lógica de inicio del sistema de cotización

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import view.MenuSistema;
+
+public class Main {
+    public static void main(String[] args){
+        MenuSistema sistema = new MenuSistema();    
+        sistema.iniciar();
+    }
+}
