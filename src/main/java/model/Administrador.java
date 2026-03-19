751ef7f76c454773ca4a0f15da8fe4387f68d664
Nath-Maya <natha.maya.ramirez93@gmail.com>
2026-03-18 19:21:56 -0500

    Agregar clase Administrador con atributos y métodos de acceso

---
+++ src/main/java/model/Administrador.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Administrador {
+
+    private String usuario;
+    private String contrasena;
+
+    public Administrador(String usuario, String contrasena){
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena(){
+        return contrasena;
+    }
+}
