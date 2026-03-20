f78e3d21376e7bca774a421211140eb61254d367
Nath-Maya <natha.maya.ramirez93@gmail.com>
2026-03-19 20:46:19 -0500

    Comentar codigo

--- src/main/java/util/Archivo.java
+++ src/main/java/util/Archivo.java
@@ -1,32 +1,39 @@
 package util;
 
+// Clase para manejar la lectura y escritura de datos en un archivo
 import java.io.*;
 import model.BaseDatos;
 
 public class Archivo{
     
+    // Nombre del archivo donde se guardarán los datos de las empresas
     private static final String NOMBRE_ARCHIVO = "empresas.dat";
     public static void guardar(BaseDatos bd){
 
+        // Guardar el objeto BaseDatos en un archivo utilizando ObjectOutputStream
         try{
             ObjectOutputStream oss =
                 new ObjectOutputStream(new FileOutputStream(NOMBRE_ARCHIVO));
 
+                // Escribir el objeto BaseDatos en el archivo
                 oss.writeObject(bd);
                 oss.close();
 
-                System.out.println("Los datos se han guardado correctamente");
+                System.out.println("Los datos se han guardado correctamente"); 
         }
+        // Manejar posibles excepciones durante la escritura del archivo
         catch (IOException e) {
             System.out.println("Error al guardar los datos");
             e.printStackTrace();
         }
     }
 
+    // Método que se encarga de cargar los datos desde el archivo y devolver un objeto BaseDatos
      public static BaseDatos cargar() {
         try {
             ObjectInputStream ois = new ObjectInputStream(new FileInputStream(NOMBRE_ARCHIVO));
 
+            // Leer el objeto Base de datos desde el archivo y devolverlo
             BaseDatos bd = (BaseDatos) ois.readObject();
             ois.close();
 
